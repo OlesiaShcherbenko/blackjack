797a6feb50f173c134417caa554c86049fdc537c
Olesia Shcherbenko <oshch96@gmail.com>
2025-05-12 15:56:32 +0200

    feat: add HTML structure; add base layout styles; add simple logic

---
+++ index.js
@@ -0,0 +1,46 @@
+let firstCard = 10;
+let secondCard = 11;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+console.log(cardsEl);
+
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 2;
+  sum += card;
+
+  cards.push(card);
+
+  renderGame();
+}
